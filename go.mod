0459081d14ad6db9277f625f882fca44c0367c26
Vasant Bajantri <vasantinnocuous@gmail.com>
2021-02-17 12:02:12 +0100

    Headings Count

--- go.mod
+++ go.mod
@@ -4,5 +4,15 @@ go 1.15
 
 require (
 	github.com/PuerkitoBio/goquery v1.6.1
+	github.com/antchfx/xmlquery v1.3.4 // indirect
+	github.com/antchfx/xpath v1.1.11 // indirect
+	github.com/gocolly/colly/v2 v2.1.0
+	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/grokify/html-strip-tags-go v0.0.1
+	github.com/steelx/extractlinks v0.0.2
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
+	golang.org/x/text v0.3.5 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,9 +6,56 @@ import (
     "net/http"
     "log"
     "os"
-    "strings"
-
+	"strings"
+	
+	"github.com/PuerkitoBio/goquery"
 )
+
+func goGet() {
+	var headings, row []string
+	var rows [][]string
+
+	data := `<html><body>
+	<table>
+		<tr><th>Marvel Heading One</th><th>Marvel Heading Two</th></tr>
+		<tr><td>Marvel 1</td><td>Marvel 2</td></tr>
+		<tr><td>Marvel 3</td><td>Marvel 4</td></tr>
+		<tr><td>Marvel 5</td><td>Marvel 6</td></tr>
+		<tr><td>marvel 7</td><td>Marvel 8</td></tr>
+	</table>
+	<p>Lets begin</p>
+	<table>
+		<tr><th>DC Heading One</th><th>DC Heading two</th></tr>
+		<tr><td>DC 1</td><td>DC 2</td></tr>
+		<tr><td>DC 3</td><td>DC 4</td></tr>
+		<tr><td>DC 5</td><td><span></span><span><a href="">DC 6</a></span></td></tr>
+		<tr><td>DC 7</td><td>DC 8</td></tr>
+	</table>
+	</body>
+	</html>
+	`
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(data))
+	if err != nil {
+		fmt.Println("No url found")
+		log.Fatal(err)
+	}
+
+	// Find each table
+	doc.Find("table").Each(func(index int, tablehtml *goquery.Selection) {
+		tablehtml.Find("tr").Each(func(indextr int, rowhtml *goquery.Selection) {
+			rowhtml.Find("th").Each(func(indexth int, tableheading *goquery.Selection) {
+				headings = append(headings, tableheading.Text())
+			})
+			rowhtml.Find("td").Each(func(indexth int, tablecell *goquery.Selection) {
+				row = append(row, tablecell.Text())
+			})
+			rows = append(rows, row)
+			row = nil
+		})
+	})
+	fmt.Println("Headings = ", len(headings), headings)
+	fmt.Println("Rows = ", len(rows), rows)
+}
 
 
 func main() {
@@ -29,7 +76,6 @@ func main() {
         fmt.Println("No title found")
         os.Exit(0)
     }
- 
     titleStartingIndex += 7
 
     titleEndingIndex := strings.Index(pageContent, "</title>")
@@ -39,6 +85,8 @@ func main() {
     }
 
     pageTitle := []byte(pageContent[titleStartingIndex:titleEndingIndex])
-
-    fmt.Printf("Page title: %s\n", pageTitle)
+	fmt.Printf("Page title: %s\n", pageTitle)
+	
+	goGet()
 }
+
